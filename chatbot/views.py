4f51ab7b3f5c1e360089d7698d0976631b5df9eb
Harsh Mand Hanu-lap <aniketkalta.hanuai@gmail.com>
2026-01-07 16:56:21 +0530

    update

--- chatbot/views.py
+++ chatbot/views.py
@@ -70,7 +70,7 @@ def start_test(request, module_id):
 # model for POSH training module
 
 from huggingface_hub import login
-login("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")
+login("hugging face api key here")
 
 # Load Phi-3-mini once at startup
 # MODEL_ID = "microsoft/phi-3-mini-4k-instruct"
